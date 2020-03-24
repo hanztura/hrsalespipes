37bb285440fbfd8bd099d891bfcd720b3687ada4
hanz <hctura.official@gmail.com>
2020-03-24 10:58:52 +0800

    change filename from viewsets.py to api.py

--- config/urls.py
+++ config/urls.py
@@ -18,7 +18,7 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
-from jobs.viewsets import JobCandidateListAPIView
+from jobs.api import JobCandidateListAPIView
 
 
 urlpatterns = [
