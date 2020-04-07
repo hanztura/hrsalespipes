d5a2f41d28158ae5ba3ad1b785a4df8df4265934
hanz tura <hanztura@github.com>
2020-04-07 08:01:43 +0800

    about static page. link on how to use hrsalespipes

--- hrsalespipes/system/urls.py
+++ hrsalespipes/system/urls.py
@@ -1,10 +1,15 @@
 from django.contrib.auth.views import LogoutView
 from django.urls import path
+from django.views.generic.base import TemplateView
 
 from .views import LoginView, HomeView, AdminRedirectView
 
 app_name = 'system'
 urlpatterns = [
+    path(
+        'about/',
+        TemplateView.as_view(template_name='system/about.html'),
+        name='about'),
     path('go-admin/', AdminRedirectView.as_view(), name='admin'),
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
