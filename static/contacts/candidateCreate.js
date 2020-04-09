9c3b03122b26243ef384a60e20c0d2681512f983
hanz tura <hanztura@github.com>
2020-04-09 21:24:50 +0800

    Better form errors. Modeled fields

--- static/contacts/candidateCreate.js
+++ static/contacts/candidateCreate.js
@@ -1,3 +1,62 @@
+let fields = {
+  contactDetails: [
+    {
+      name: 'name',
+      label: 'Name',
+      value: data.name,
+      fieldType: {
+        value: 'textfield',
+        type: 'text'
+      },
+      outlined: true,
+      rules: [
+        v => !!v || 'Name is required',
+      ]
+    },
+
+    {
+      name: 'location',
+      label: 'Location',
+      items: 'locations',
+      itemText: 'text',
+      itemValue: 'text',
+      model: 'location',
+      fieldType: {
+        value: 'autocomplete',
+      },
+      dense: true,
+      outlined: true,
+      rules: []
+    },
+
+    {
+      name: 'contact_number',
+      label: 'Contact Number',
+      value: data.contactNumber,
+      fieldType: {
+        value: 'textfield',
+        type: 'text'
+      },
+      outlined: true,
+      rules: []
+    },
+
+    {
+      name: 'email_address',
+      label: 'Email Address',
+      value: data.emailAddress,
+      fieldType: {
+        value: 'textfield',
+        type: 'email'
+      },
+      outlined: true,
+      rules: [
+        v => v == '' || /.+@.+\..+/.test(v) || 'E-mail must be in a valid format',
+      ]
+    },
+  ] ,
+}
+
 new Vue({
   el: '#inspire',
   vuetify: new Vuetify(),
@@ -20,8 +79,9 @@ new Vue({
         disabled: true,
       }
     ],
+    fields: fields,
     locations: [],
-    location: '',
+    location: data.location,
   }),
 
   beforeMount(){
