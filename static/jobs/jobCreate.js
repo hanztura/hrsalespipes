77e9ed98300fc6847e67d089c75d57bbcb0c0652
hanz tura <hanztura@github.com>
2020-04-09 22:37:56 +0800

    Better form errors. Use modeled fields

--- static/jobs/jobCreate.js
+++ static/jobs/jobCreate.js
@@ -1,3 +1,51 @@
+let fields = [
+  {
+    name: 'reference_number',
+    label: data.referenceNumberLabel,
+    value: data.referenceNumber,
+    fieldType: {
+      value: 'textfield',
+      type: 'text'
+    },
+    outlined: true,
+    rules: [
+      v => !!v || 'Reference number is required',
+    ]
+  },
+
+  {
+    name: 'client',
+    label: 'Client',
+    items: 'clients',
+    itemText: 'text',
+    itemValue: 'value',
+    model: 'client',
+    fieldType: {
+      value: 'autocomplete',
+    },
+    dense: true,
+    outlined: true,
+    rules: [
+      v => !!v || 'Client is required',
+    ]
+  },
+
+  {
+    name: 'position',
+    label: 'Position',
+    value: data.position,
+    fieldType: {
+      value: 'textfield',
+      type: 'text'
+    },
+    outlined: true,
+    rules: [
+      v => !!v || 'Position is required',
+    ]
+  },
+
+];
+
 new Vue({
   el: '#inspire',
   vuetify: new Vuetify(),
@@ -15,6 +63,7 @@ new Vue({
         disabled: true,
       }
     ],
+    fields: fields,
     clients: [],
     client: data.client,
   }),
