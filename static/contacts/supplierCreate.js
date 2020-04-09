f12602656f4c470b58123ee6bfeeed6eeaa5ff55
hanz tura <hanztura@github.com>
2020-04-09 09:59:49 +0800

    Better form errors. Use model for creating fields

--- static/contacts/supplierCreate.js
+++ static/contacts/supplierCreate.js
@@ -1,3 +1,43 @@
+let fields = [
+  {
+    name: 'name',
+    label: 'Name',
+    value: data.name,
+    fieldType: {
+      value: 'textfield',
+      type: 'text'
+    },
+    outlined: true,
+    rules: [
+      v => !!v || 'Name is required',
+    ]
+  },
+  {
+    name: 'contact_number',
+    label: 'Contact Number',
+    value: data.contactNumber,
+    fieldType: {
+      value: 'textfield',
+      type: 'text'
+    },
+    outlined: true,
+    rules: []
+  },
+  {
+    name: 'email_address',
+    label: 'Email Address',
+    value: data.emailAddress,
+    fieldType: {
+      value: 'textfield',
+      type: 'email'
+    },
+    outlined: true,
+    rules: [
+      v => v == '' || /.+@.+\..+/.test(v) || 'E-mail must be in a valid format',
+    ]
+  },
+]
+
 new Vue({
   el: '#inspire',
   vuetify: new Vuetify(),
@@ -20,6 +60,7 @@ new Vue({
         disabled: true,
       }
     ],
+    fields: fields,
   }),
 
   mounted(){
