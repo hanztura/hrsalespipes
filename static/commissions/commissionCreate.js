f482c70f32a128b4e3a1a58971a3500b7fa21d69
hanz tura <hanztura@github.com>
2020-04-10 08:32:35 +0800

    better form errors. use modeled fields.

--- static/commissions/commissionCreate.js
+++ static/commissions/commissionCreate.js
@@ -1,3 +1,74 @@
+let fields = [
+
+    {
+      name: 'employee',
+      label: 'Employee',
+      items: 'employees',
+      itemText: 'text',
+      itemValue: 'value',
+      model: 'employee',
+      disabled: false,
+      fieldType: {
+        value: 'autocomplete',
+      },
+      dense: true,
+      outlined: true,
+      rules: [
+        v => (v != '' && !!v) || 'Employee is required',
+      ]
+    },
+
+    {
+      name: 'rate_role_type',
+      label: 'Rate Role Type',
+      items: 'rateRoleTypes',
+      itemText: 'text',
+      itemValue: 'value',
+      model: 'rateRoleType',
+      disabled: false,
+      fieldType: {
+        value: 'autocomplete',
+      },
+      dense: true,
+      outlined: true,
+      rules: [
+        v => !!v || 'Rate Role Type is required',
+      ]
+    },
+
+    {
+      name: 'rate_used',
+      label: 'Rate Used',
+      model: 'rateUsed',
+      fieldType: {
+        value: 'textfieldModeled',
+        type: 'number',
+        step: '0.01'
+      },
+      outlined: true,
+      rules: [
+        v => !!v || 'Rate Used is required',
+      ]
+    },
+
+    {
+      name: 'amount',
+      label: 'Amount',
+      model: 'amount',
+      fieldType: {
+        value: 'textfieldModeled',
+        type: 'number',
+        step: '0.01'
+      },
+      outlined: true,
+      rules: [
+        v => !!v || 'Amount is required',
+      ]
+    },
+
+
+];
+
 new Vue({
   el: '#inspire',
   vuetify: new Vuetify(),
@@ -15,10 +86,13 @@ new Vue({
         disabled: true,
       }
     ],
+    fields: fields,
     employees: [],
     employee: data.employee,
     rateRoleTypes: data.rateRoleTypes,
     rateRoleType: data.rateRoleType,
+    rateUsed: data.rateUsed,
+    amount: data.amount,
   }),
 
   beforeMount(){
--- static/commissions/commissionUpdate.js
+++ static/commissions/commissionUpdate.js
@@ -1,3 +1,98 @@
+let fields = [
+
+    {
+      name: 'date',
+      label: 'date',
+      model: 'date',
+      fieldType: {
+        value: 'datetextfield',
+        type: 'date'
+      },
+      outlined: true,
+      rules: [
+        v => !!v || 'Date is required',
+      ]
+    },
+
+    {
+      name: 'employee',
+      label: 'Employee',
+      items: 'employees',
+      itemText: 'text',
+      itemValue: 'value',
+      model: 'employee',
+      disabled: false,
+      fieldType: {
+        value: 'autocomplete',
+      },
+      dense: true,
+      outlined: true,
+      rules: [
+        v => !!v || 'Employee is required',
+      ]
+    },
+
+    {
+      name: 'rate_role_type',
+      label: 'Rate Role Type',
+      items: 'rateRoleTypes',
+      itemText: 'text',
+      itemValue: 'value',
+      model: 'rateRoleType',
+      disabled: false,
+      fieldType: {
+        value: 'autocomplete',
+      },
+      dense: true,
+      outlined: true,
+      rules: [
+        v => !!v || 'Rate Role Type is required',
+      ]
+    },
+
+    {
+      name: 'rate_used',
+      label: 'Rate Used',
+      model: 'rateUsed',
+      fieldType: {
+        value: 'textfieldModeled',
+        type: 'number',
+        step: '0.01'
+      },
+      outlined: true,
+      rules: [
+        v => !!v || 'Rate Used is required',
+      ]
+    },
+
+    {
+      name: 'amount',
+      label: 'Amount',
+      model: 'amount',
+      fieldType: {
+        value: 'textfieldModeled',
+        type: 'number',
+        step: '0.01'
+      },
+      outlined: true,
+      rules: [
+        v => !!v || 'Amount is required',
+      ]
+    },
+
+    {
+      name: 'is_paid',
+      label: 'Is Paid?',
+      model: 'isPaid',
+      fieldType: {
+        value: 'checkbox',
+      },
+      outlined: true,
+      rules: []
+    },
+
+];
+
 new Vue({
   el: '#inspire',
   vuetify: new Vuetify(),
@@ -19,11 +114,15 @@ new Vue({
         text: 'Edit',
       }
     ],
+    fields: fields,
     employees: [],
     employee: data.employee,
     rateRoleTypes: data.rateRoleTypes,
     rateRoleType: data.rateRoleType,
     isPaid: data.isPaid,
+    date: data.date,
+    rateUsed: data.rateUsed,
+    amount: data.amount,
   }),
 
   beforeMount(){
