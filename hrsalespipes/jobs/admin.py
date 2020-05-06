8088118772375f8265989949f4a839643036cc0b
hanz tura <hanztura@github.com>
2020-05-07 00:55:35 +0800

    New admin actions for Job records. Update job status actions

--- hrsalespipes/jobs/admin.py
+++ hrsalespipes/jobs/admin.py
@@ -1,10 +1,24 @@
-from django.contrib import admin
+from django.contrib import admin, messages
 
 from import_export import resources
 from import_export.admin import ImportExportModelAdmin
 from simple_history.admin import SimpleHistoryAdmin
 
 from .models import Status, JobStatus, Job, JobCandidate, Interview
+
+
+def make_change_job_status(status):
+    def change_job_status(modeladmin, request, queryset):
+        queryset.update(status=status)
+        messages.info(request, 'Updated {} Job status to {}'.format(
+            queryset.count(),
+            status.name))
+
+    change_job_status.short_description = 'Change Job Status to {}'.format(
+        status.name)
+    change_job_status.__name__ = 'change_job_status_{}'.format(status.pk)
+    change_job_status.allowed_permissions = ('change',)
+    return change_job_status
 
 
 class JobResource(resources.ModelResource):
@@ -27,6 +41,18 @@ class JobAdmin(
         SimpleHistoryAdmin):
     resource_class = JobResource
     readonly_fields = JobResource.Meta.fields
+    list_display = ['id', 'reference_number', 'date', 'status']
+
+    def get_actions(self, request):
+        actions = super().get_actions(request)
+
+        for status in JobStatus.objects.all():
+            action = make_change_job_status(status)
+            actions[action.__name__] = (action,
+                                        action.__name__,
+                                        action.short_description)
+
+        return actions
 
 
 class JobCandidateAdmin(SimpleHistoryAdmin):
