77accf3132726ced05d84fa2a41891b841f310b8
hanz <hctura.official@gmail.com>
2020-11-11 18:23:12 +0800

    read .env

--- config/wsgi.py
+++ config/wsgi.py
@@ -9,12 +9,15 @@ https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 
 import os
 import sys
+import dotenv
 
 from django.core.wsgi import get_wsgi_application
 
+project_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+dotenv.read_dotenv(os.path.join(project_path, '.env'))
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.production')
 
-project_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append(os.path.join(project_path, 'hrsalespipes'))
 
 application = get_wsgi_application()
